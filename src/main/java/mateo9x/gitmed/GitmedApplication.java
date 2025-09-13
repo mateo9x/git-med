506b3e515e3e6cf41b2761f90ede6f9b9a861c00
Mateusz <mtpala@interia.pl>
2025-09-13 21:51:42 +0200

    Init commit

---
+++ src/main/java/mateo9x/gitmed/GitmedApplication.java
@@ -0,0 +1,13 @@
+package mateo9x.gitmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitmedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitmedApplication.class, args);
+    }
+
+}
